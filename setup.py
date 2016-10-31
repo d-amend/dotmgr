9efb71ddf11c85710736bb88c208f92737aa2683
Sebastian Neuser <haggl@sineband.de>
2016-10-31 15:50:15 +0100

    Version 0.2.4 \o/

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='haggl@sineband.de',
     url='https://github.com/haggl/dotmgr',
     license='GPLv3+',
-    version='0.2.3',
+    version='0.2.4',
     packages=find_packages(),
     scripts=['dotmgr/dotmgr'],
     install_requires=['gitpython']
